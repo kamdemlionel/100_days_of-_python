11e2be81a3c2fa7ccc3c44f38099e08481ad14c5
kamdemlionel <kamdemlionel542@gmail.com>
2025-08-16 22:27:28 +0100

    Initial commit

---
+++ task.py
@@ -0,0 +1,10 @@
+#welcome for the program
+print("welcome to the band name generator ")
+#asking the user in which city they grew in 
+city=input("in which city did you grew in ?\n")
+#asking the user the name of the pet
+pet=input("what is the name of your pet ?\n")
+#creating the band name
+band_name=city+" "+pet
+#displaying the band name
+print("your band name could be "+band_name)
