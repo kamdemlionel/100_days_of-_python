17461a433d463cd68b922d7996e9705b88723a5b
kamdemlionel <kamdemlionel542@gmail.com>
2025-08-18 23:48:00 +0100

    add day3.py (Treasure Island game)

---
+++ day3.py
@@ -0,0 +1,39 @@
+print('''
+*******************************************************************************
+          |                   |                  |                     |
+ _________|________________.=""_;=.______________|_____________________|_______
+|                   |  ,-"_,=""     `"=.|                  |
+|___________________|__"=._o`"-._        `"=.______________|___________________
+          |                `"=._o`"=._      _`"=._                     |
+ _________|_____________________:=._o "=._."_.-="'"=.__________________|_______
+|                   |    __.--" , ; `"=._o." ,-"""-._ ".   |
+|___________________|_._"  ,. .` ` `` ,  `"-._"-._   ". '__|___________________
+          |           |o`"=._` , "` `; .". ,  "-._"-._; ;              |
+ _________|___________| ;`-.o`"=._; ." ` '`."\` . "-._ /_______________|_______
+|                   | |o;    `"-.o`"=._``  '` " ,__.--o;   |
+|___________________|_| ;     (#) `-.o `"=.`_.--"_o.-; ;___|___________________
+____/______/______/___|o;._    "      `".o|o_.--"    ;o;____/______/______/____
+/______/______/______/_"=._o--._        ; | ;        ; ;/______/______/______/_
+____/______/______/______/__"=._o--._   ;o|o;     _._;o;____/______/______/____
+/______/______/______/______/____"=._o._; | ;_.--"o.--"_/______/______/______/_
+____/______/______/______/______/_____"=.o|o_.--""___/______/______/______/____
+/______/______/______/______/______/______/______/______/______/______/_____ /
+*******************************************************************************
+''')
+print("Welcome to Treasure Island.")
+print("Your mission is to find the treasure.") 
+direction= input("you are at a crossroad. where do you want to go? type left or right:\n")
+if direction=="left":
+    decision=input("you came into a lake, there is a n island in the middle of the lake. type wait to wait a boat or type swim to swim accross:\n")
+    if decision=="wait":
+        room=input("you arrived at the island. there is a house with three doors, red, blue and yellow. which colour do you choose:\n")
+        if room=="yellow":
+            print("you win")
+        elif room=="blue":
+            print("eaten by beast\n game over")
+        elif room=="red":
+            print("burned by fire\n game over")
+    else:
+        print("attacked by trout\n game over")
+else:
+    print("fall into a hole\n game over")
